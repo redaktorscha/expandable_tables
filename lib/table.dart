2ca43adf06c232fbbb7df6fadb869b20dca8197f
Masha <nasonkina@yandex.ru>
2024-10-16 17:36:54 +0300

    add example

--- lib/table.dart
+++ lib/table.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+// ignore: depend_on_referenced_packages
 import 'package:flutter_expandable_table/flutter_expandable_table.dart';
-import './custom_row.dart';
-import './cell_card.dart';
+
+const Color _primaryColor = Color(0xFF1e2f36); //corner
+const Color _accentColor = Color(0xFF0d2026); //background
+const TextStyle _textStyle = TextStyle(color: Colors.white);
+// const TextStyle _textStyleSubItems = TextStyle(color: Colors.grey);
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -11,225 +17,39 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => MyHomePageState();
 }
 
-class MyHomePageState extends State<MyHomePage> {
-  List<CustomRow> tableRows = [
-    CustomRow(
-        index: 0,
-        depth: 0,
-        parentIndex: null,
-        expanded: false,
-        expandable: true,
-        visible: true,
-        label: 'Row 1'),
-    CustomRow(
-        index: 1,
-        depth: 1,
-        parentIndex: 0,
-        visible: false,
-        expanded: false,
-        expandable: false,
-        label: 'Row 1.1'),
-    CustomRow(
-        index: 2,
-        depth: 1,
-        parentIndex: 0,
-        visible: false,
-        expanded: false,
-        expandable: false,
-        label: 'Row 1.2'),
-    CustomRow(
-        index: 3,
-        depth: 0,
-        parentIndex: null,
-        visible: true,
-        expanded: false,
-        expandable: true,
-        label: 'Row 2'),
-    CustomRow(
-        index: 4,
-        depth: 1,
-        parentIndex: 3,
-        visible: false,
-        expanded: false,
-        expandable: true,
-        label: 'Row 2.1'),
-    CustomRow(
-        index: 5,
-        depth: 2,
-        parentIndex: 4,
-        visible: false,
-        expanded: false,
-        expandable: false,
-        label: 'Row 2.1.1'),
-    CustomRow(
-        index: 6,
-        depth: 2,
-        parentIndex: 4,
-        visible: false,
-        expanded: false,
-        expandable: false,
-        label: 'Row 2.1.2'),
-    CustomRow(
-        index: 7,
-        depth: 2,
-        parentIndex: 4,
-        visible: false,
-        expanded: false,
-        expandable: false,
-        label: 'Row 2.1.3'),
-    CustomRow(
-        index: 8,
-        depth: 1,
-        parentIndex: 3,
-        visible: false,
-        expanded: false,
-        expandable: false,
-        label: 'Row 2.2'),
-    CustomRow(
-        index: 9,
-        parentIndex: null,
-        visible: true,
-        depth: 0,
-        expanded: false,
-        expandable: false,
-        label: 'Row 3'),
-    CustomRow(
-        index: 10,
-        parentIndex: null,
-        depth: 0,
-        expanded: false,
-        expandable: true,
-        visible: true,
-        label: 'Row 4'),
-    CustomRow(
-        index: 11,
-        parentIndex: 10,
-        depth: 1,
-        expanded: false,
-        expandable: false,
-        visible: false,
-        label: 'Row 4.1'),
-    CustomRow(
-        index: 12,
-        parentIndex: 10,
-        depth: 1,
-        expanded: false,
-        expandable: false,
-        visible: false,
-        label: 'Row 4.2'),
-    CustomRow(
-        index: 13,
-        parentIndex: 10,
-        visible: false,
-        depth: 1,
-        expanded: false,
-        expandable: false,
-        label: 'Row 4.3'),
-    CustomRow(
-        index: 14,
-        parentIndex: 10,
-        visible: false,
-        depth: 1,
-        expanded: false,
-        expandable: false,
-        label: 'Row 4.4'),
-    CustomRow(
-        index: 15,
-        parentIndex: null,
-        visible: true,
-        depth: 0,
-        expanded: false,
-        expandable: true,
-        label: 'Row 5'),
-    CustomRow(
-        index: 16,
-        parentIndex: 15,
-        visible: false,
-        depth: 1,
-        expanded: false,
-        expandable: true,
-        label: 'Row 5.1'),
-    CustomRow(
-        index: 17,
-        parentIndex: 16,
-        visible: false,
-        depth: 2,
-        expanded: false,
-        expandable: false,
-        label: 'Row 5.1.1'),
-    CustomRow(
-        index: 18,
-        parentIndex: 16,
-        visible: false,
-        depth: 2,
-        expanded: false,
-        expandable: false,
-        label: 'Row 5.1.2'),
-    CustomRow(
-        index: 19,
-        parentIndex: 15,
-        visible: false,
-        depth: 1,
-        expanded: false,
-        expandable: true,
-        label: 'Row 5.2'),
-    CustomRow(
-        index: 20,
-        parentIndex: 19,
-        visible: false,
-        depth: 2,
-        expanded: false,
-        expandable: false,
-        label: 'Row 5.2.1'),
-    CustomRow(
-        index: 21,
-        parentIndex: 19,
-        visible: false,
-        depth: 2,
-        expanded: false,
-        expandable: false,
-        label: 'Row 5.2.2'),
-  ];
+class _DefaultCellCard extends StatelessWidget {
+  final Widget child;
 
-  List <CustomRow> renderRows = [];
-  List<ExpandableTableHeader> headers = [];
-  final int columnsCount = 20;
-  final int subRowsCount = 2;
+  const _DefaultCellCard({
+    required this.child,
+  });
 
   @override
-  void initState() {
-    setState(() {
-      renderRows = [...tableRows].where((row) => row.visible).toList();
-      headers = List.generate(
-        columnsCount - 1,
-        (index) => ExpandableTableHeader(
-          width: index % 2 == 0 ? 200 : 150,
-          cell: buildCell('Column $index'),
-        ),
+  Widget build(BuildContext context) => Container(
+        color: _primaryColor,
+        margin: const EdgeInsets.all(1),
+        child: child,
       );
-    });
+}
 
-    super.initState();
-  }
-
-  ExpandableTableCell buildCell(String content, {CellBuilder? builder}) {
-    return ExpandableTableCell(
-      child: builder != null
-          ? null
-          : DefaultCellCard(
-              child: Center(
-                child: Text(
-                  content,
-                  style: TextStyle(color: Colors.white),
+class MyHomePageState extends State<MyHomePage> {
+  ExpandableTableCell _buildCell(String content, {CellBuilder? builder}) =>
+      ExpandableTableCell(
+        child: builder != null
+            ? null
+            : _DefaultCellCard(
+                child: Center(
+                  child: Text(
+                    content,
+                    style: _textStyle,
+                  ),
                 ),
               ),
-            ),
-      builder: builder,
-    );
-  }
+        builder: builder,
+      );
 
-  ExpandableTableCell buildFirstRowCell() => ExpandableTableCell(
-        builder: (context, details) => DefaultCellCard(
+  ExpandableTableCell _buildFirstRowCell() => ExpandableTableCell(
+        builder: (context, details) => _DefaultCellCard(
           child: Padding(
             padding: const EdgeInsets.only(left: 16.0),
             child: Row(
@@ -254,6 +74,7 @@ class MyHomePageState extends State<MyHomePage> {
                 ),
                 Text(
                   '${details.row!.address.length > 1 ? details.row!.address.skip(1).map((e) => 'Sub ').join() : ''}Row ${details.row!.address.last}',
+                  style: _textStyle,
                 ),
               ],
             ),
@@ -261,48 +82,105 @@ class MyHomePageState extends State<MyHomePage> {
         ),
       );
 
-  List<ExpandableTableRow> generateRows(int quantity, {int depth = 0}) {
+  static const int columnsCount = 20;
+  static const int subColumnsCount = 2;
+  static const int rowsCount = 6;
+  static const int subRowsCount = 3;
+  static const int totalColumns = columnsCount + subColumnsCount;
+
+  List<ExpandableTableRow> _generateRows(int quantity, {int depth = 0}) {
     final bool generateLegendRow = (depth == 0 || depth == 2);
-    return renderRows.map((row) => ExpandableTableRow(
-        firstCell: buildFirstRowCell(),
-        children: ((row.index < renderRows.length) && depth < 3)
-            ? generateRows(subRowsCount, depth: depth + 1)
+    return List.generate(
+      quantity,
+      (rowIndex) => ExpandableTableRow(
+        firstCell: _buildFirstRowCell(),
+        children: ((rowIndex == 3 || rowIndex == 2) && depth < 3)
+            ? _generateRows(subRowsCount, depth: depth + 1)
             : null,
-        cells: 
-             List<ExpandableTableCell>.generate(
-                columnsCount,
-                (columnIndex) => buildCell('${row.label}- cell $columnIndex'),
-              ),
-        legend: null,
+        cells: !(generateLegendRow && (rowIndex == 3 || rowIndex == 2))
+            ? List<ExpandableTableCell>.generate(
+                totalColumns,
+                (columnIndex) => _buildCell('Cell $rowIndex:$columnIndex'),
+              )
+            : null,
+        legend: generateLegendRow && (rowIndex == 3 || rowIndex == 2)
+            ? const _DefaultCellCard(
+                child: Align(
+                  alignment: FractionalOffset.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 24.0),
+                    child: Text(
+                      'This is row legend',
+                      style: _textStyle,
+                    ),
+                  ),
+                ),
+              )
+            : null,
       ),
-    ).toList();
+    );
   }
 
+  ExpandableTable _buildExpandableTable() {
+    //Creation header
+    final List<ExpandableTableHeader> subHeader = List.generate(
+      subColumnsCount,
+      (index) => ExpandableTableHeader(
+        cell: _buildCell('Sub Column $index'),
+      ),
+    );
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('expandable table')),
-        body: Row(children: [
-          Expanded(
-              child: SingleChildScrollView(
-                  child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: buildExpandableTable())))
-        ]));
-  }
+    //Creation header
+    final List<ExpandableTableHeader> headers = List.generate(
+      columnsCount,
+      (index) => ExpandableTableHeader(
+          cell: _buildCell(
+              '${index == 1 ? 'Expandable\nColumn' : 'Column'} $index'),
+          children: index == 1 ? subHeader : null),
+    );
 
-  ExpandableTable buildExpandableTable() {
     return ExpandableTable(
-      firstHeaderCell: buildCell('Simple\nTable'),
-      rows: generateRows(renderRows.length),
+      firstHeaderCell: _buildCell('Expandable\nTable'),
+      rows: _generateRows(rowsCount),
       headers: headers,
-      // defaultsRowHeight: 60,
-      // defaultsColumnWidth: 150,
-      // firstColumnWidth: 250,
-      scrollShadowColor: Colors.grey,
+      defaultsRowHeight: 60,
+      defaultsColumnWidth: 150,
+      firstColumnWidth: 250,
+      scrollShadowColor: _accentColor,
       visibleScrollbar: true,
       expanded: false,
     );
   }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text(
+              '   Simple Table                    |                    Expandable Table'),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: _accentColor,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: _buildExpandableTable(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class _AppCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
+      };
 }
